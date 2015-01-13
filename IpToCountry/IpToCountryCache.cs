f958187c7c180eae490aa3c6bdb2ded32bc139e6
Regnārs Breikšs <rbreikss@gmail.com>
2015-01-13 16:52:53 +0200

    Automatic initialization of cache added. Cache property made public.

--- IpToCountry/IpToCountryCache.cs
+++ IpToCountry/IpToCountryCache.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using SpartanExtensions;
 using CsvHelper;
 using IP2Location;
@@ -11,7 +10,7 @@ namespace IpToCountry
 {
     public static class IpToCountryCache
     {
-        internal static List<IPRangeCountry> Cache;
+        public static List<IPRangeCountry> Cache;
 
         public static void Load()
         {
@@ -33,20 +32,29 @@ namespace IpToCountry
         public static IpAddressLocation GetIpAddressLocation(IPAddress ipAddress)
         {
             var ipAddressIntegerRepresentation = ipAddress.ToInteger();
+
+            InitCache();
+
             var correspondingRange = Cache.FirstOrDefault(r => r.StartIP <= ipAddressIntegerRepresentation
                                                                && r.EndIP >= ipAddressIntegerRepresentation);
-            return correspondingRange != null ? 
-                new IpAddressLocation(correspondingRange.ISO_Code_2) 
+            return correspondingRange != null ?
+                new IpAddressLocation(correspondingRange.ISO_Code_2)
                 : new IpAddressLocation();
         }
 
-        private static async Task LoadCountryRanges(string csvDatabasePath)
+        private static void InitCache()
+        {
+            if (Cache == null)
+                Load();
+        }
+
+        private static void LoadCountryRanges(string csvDatabasePath)
         {
             var csv = new CsvReader(File.OpenText(csvDatabasePath));
             Cache = csv.GetRecords<IPRangeCountry>().ToList();
         }
 
-        private static async Task SaveCountryRanges(string csvDatabasePath, IEnumerable<IPRangeCountry> ranges)
+        private static void SaveCountryRanges(string csvDatabasePath, IEnumerable<IPRangeCountry> ranges)
         {
             var csv = new CsvWriter(File.CreateText(csvDatabasePath));
             csv.WriteRecords(ranges);
